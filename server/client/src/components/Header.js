abecef732ac86b8b288b665e4b89e0fe71d364be
Dan Buda <danbuda@gmail.com>
2017-08-10 12:33:50 -0500

    finish header login/logout routing

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return (
+					<li>
+						<a href="/auth/google">Login With Google</a>
+					</li>
+				);
+			default:
+				return (
+					<li>
+						<a href="/api/logout">Logout</a>
+					</li>
+				);
+		}
+	}
+
 	render() {
 		return (
 			<nav>
 				<div className="nav-wrapper">
 					<a className="left brand-logo">Emaily</a>
 					<ul className="right">
-						<li>
-							<a>Login with Google</a>
-						</li>
+						{this.renderContent()}
 					</ul>
 				</div>
 			</nav>
@@ -17,4 +35,8 @@ class Header extends Component {
 	}
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
