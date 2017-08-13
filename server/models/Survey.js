99d8c4be5b612336f7d107272cf63778217e6dae
Dan Buda <danbuda@gmail.com>
2017-08-12 22:58:38 -0500

    created Survey model

---
+++ server/models/Survey.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const surveySchema = new Schema({
+	title: String,
+	subject: String,
+	body: String,
+	recipients: [String],
+	yes: { type: Number, default: 0 },
+	no: { type: Number, default: 0 }
+});
+
+mongoose.model('surveys', surveySchema);
