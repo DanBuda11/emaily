c1ea0c8a078e426f500ec69f41fcdfb0b23b25f1
Dan Buda <danbuda@gmail.com>
2017-08-18 18:21:30 -0500

    more work on survey fields

--- server/client/src/components/surveys/SurveyField.js
+++ server/client/src/components/surveys/SurveyField.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-export default ({ input }) => {
+export default ({ input, label }) => {
 	return (
 		<div>
+			<label>
+				{label}
+			</label>
 			<input {...input} />
 		</div>
 	);
