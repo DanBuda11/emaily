66366533f20ac6fe08963df8e141f93a0acf14b0
Dan Buda <danbuda@gmail.com>
2017-08-19 16:34:42 -0500

    add form validation

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -3,11 +3,24 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
-	{ label: 'Survey Title', name: 'title' },
-	{ label: 'Subject Line', name: 'subject' },
-	{ label: 'Email Body', name: 'body' },
+	{
+		label: 'Survey Title',
+		name: 'title',
+		noValueError: 'You must provide a title'
+	},
+	{
+		label: 'Subject Line',
+		name: 'subject',
+		noValueError: 'You must provide a subject'
+	},
+	{
+		label: 'Email Body',
+		name: 'body',
+		noValueError: 'You must provide an email body'
+	},
 	{ label: 'Recipient List', name: 'emails' }
 ];
 
@@ -44,6 +57,21 @@ class SurveyForm extends Component {
 	}
 }
 
+function validate(values) {
+	const errors = {};
+
+	errors.emails = validateEmails(values.emails || '');
+
+	_.each(FIELDS, ({ name, noValueError }) => {
+		if (!values[name]) {
+			errors[name] = noValueError;
+		}
+	});
+
+	return errors;
+}
+
 export default reduxForm({
+	validate,
 	form: 'surveyForm'
 })(SurveyForm);
